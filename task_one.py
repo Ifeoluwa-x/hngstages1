bd4dbdc7a84126eaa7b2e7d5e95c28e7adf5eb29
Aribo ifeoluwa <98714449+Ifeoluwa-x@users.noreply.github.com>
2022-10-30 18:53:55 +0100

    Add files via upload

---
+++ task_one.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route("/ifeoluwa",  methods=['GET'])
+def home():
+    return jsonify({"slackUsername":"Aribo Ifeoluwa", "backend":True, "age":24, "bio":"Hi, My name is Ifeoluwa and i'm a backend developer."})
+
+if __name__ == "__main__":
+    app.run(debug=True)
