b6d8a2609ef08f6b10b69ec5ca2a8b9ae7a481d2
Aribo ifeoluwa <98714449+Ifeoluwa-x@users.noreply.github.com>
2022-10-30 23:49:33 +0100

    Update task_one.py

--- task_one.py
+++ task_one.py
@@ -1,13 +1,12 @@
 import os
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
 @app.route("/", methods=['GET'])
 def home():
-    return jsonify({"slackUsername":"ifeoluwa_x", "backend":True, "age":24, "bio":"Hi, My name is Ifeoluwa and i'm a backend developer."})
+    return jsonify({"slackUsername":"ifeoluwa_x",  "backend":True,  "age":24,  "bio":"Hi, My name is Ifeoluwa and i'm a backend developer."})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
